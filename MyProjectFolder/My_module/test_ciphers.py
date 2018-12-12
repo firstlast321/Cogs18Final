e1c52680be20d21f106f33a17e529aecb2cbecf8
firstlast321 <45804299+firstlast321@users.noreply.github.com>
2018-12-11 16:51:56 -0800

    Delete test_ciphers.py

--- MyProjectFolder/My_module/test_ciphers.py
+++
@@ -1,7 +0,0 @@
-from ciphers import *
-
-def test_encode_message():
-    
-    assert callable(encode_message) # This tests that it is a function
-    assert isinstance(encode_message('test'), str) # testing if output is correct type
-    assert encode_message('abcde') == 'efghi' # testing for input and output
